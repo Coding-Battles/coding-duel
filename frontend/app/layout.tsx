bbb65f103d8e9b3161f9997508abf14211021c30
Andrew Kim <andrewkim09@hotmail.com>
2025-09-30 16:15:41 -0400

    added Suspense wrapper in root layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Suspense } from "react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -28,15 +29,17 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
-        <ThemeProvider 
-          attribute="class" 
-          defaultTheme="dark"
-          forcedTheme="dark"
-          enableSystem={false}
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ThemeProvider 
+            attribute="class" 
+            defaultTheme="dark"
+            forcedTheme="dark"
+            enableSystem={false}
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </Suspense>
       </body>
     </html>
   );
