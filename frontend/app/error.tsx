4fd689f457ce4da702a92669401063c3869f7dc4
Andrew Kim <andrewkim09@hotmail.com>
2025-09-30 20:55:02 -0400

    added several error catchers

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -1,3 +1,17 @@
-export default function NotFound() {
-     return <div>Error - Something went wrong</div>;
+'use client'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h2>Error - Something went wrong</h2>
+      <p>{error.message}</p>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
 }
