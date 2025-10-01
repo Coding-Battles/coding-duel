ebac30d8e0fc3eedbab213b43b24dcf9c1fd6d74
Andrew Kim <andrewkim09@hotmail.com>
2025-10-01 01:05:23 -0400

    remove themewrapper test

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Suspense } from "react";
-import ThemeWrapper from "@/components/ThereWrapper";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,12 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
-      >
-        <Suspense fallback={<div>Loading...</div>}>
-          <ThemeWrapper>{children}</ThemeWrapper>
-        </Suspense>
+      <body className={`${inter.variable} ${geistMono.variable}`}>
+        {children}
       </body>
     </html>
   );
