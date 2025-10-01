0fb009f06419a048a0c77a493e9f098121d88d9f
Andrew Kim <andrewkim09@hotmail.com>
2025-10-01 12:10:54 -0400

    re added suspense and theme wrapper

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
+import ThemeWrapper from "@/components/ThereWrapper";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,8 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} ${geistMono.variable}`}>
-        {children}
+      <body
+        className={`${inter.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <ThemeWrapper>{children}</ThemeWrapper>
+        </Suspense>
       </body>
     </html>
   );
